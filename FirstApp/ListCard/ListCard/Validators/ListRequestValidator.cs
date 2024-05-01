833cdcea76404428fd38a57784ba2f78bb92cf9a
Kyrylo Zolotarov <zlatashay@gmail.com>
2024-05-01 21:58:30 +0300

    fluent validation rules added to ListCard microservice

---
+++ FirstApp/ListCard/ListCard/Validators/ListRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using ListCard.Data.Requests;
+
+namespace ListCard.Validators
+{
+    public class ListRequestValidator : AbstractValidator<ListRequest>
+    {
+        public ListRequestValidator()
+        {
+            RuleFor(h => h.UserId).NotEmpty();
+            RuleFor(x => x.Title).MaximumLength(30);
+        }
+    }
+}
