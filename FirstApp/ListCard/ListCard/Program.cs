c872727a23e0cb00cd60f94c71e2add9517e9650
Kyrylo Zolotarov <zlatashay@gmail.com>
2024-05-01 21:50:47 +0300

    program.cs of ListCard microservice filled

--- FirstApp/ListCard/ListCard/Program.cs
+++ FirstApp/ListCard/ListCard/Program.cs
@@ -1,15 +1,47 @@
+using FluentValidation.AspNetCore;
+using Infrastructure.Services.Interfaces;
+using Infrastructure.Services;
+using ListCard.Repositories;
+using ListCard.Repositories.Interfaces;
+using ListCard.Services;
+using ListCard.Services.Interfaces;
+
+var configuration = GetConfiguration();
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddTransient<IListService, ListService>();
+builder.Services.AddTransient<IListRepository, ListRepository>();
+builder.Services.AddTransient<ICardService, CardService>();
+builder.Services.AddTransient<ICardRepository, CardRepository>();
+builder.Services.AddHttpClient();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<IHttpClientService, HttpClientService>();
+builder.Services.AddControllers();
+builder.Services.AddFluentValidationAutoValidation().AddFluentValidationClientsideAdapters();
+builder.Services.AddOptions();
+builder.Services.AddMemoryCache();
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapDefaultControllerRoute();
+    endpoints.MapControllers();
+});
 
 app.Run();
+
+IConfiguration GetConfiguration()
+{
+    var builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", false, true)
+        .AddEnvironmentVariables();
+
+    return builder.Build();
+}
